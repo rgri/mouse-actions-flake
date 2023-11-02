a4048ec2279ffea473a53e43c24593c49e1c318b
rgri <robert13grimley@gmail.com>
2023-11-02 04:30:46 -0500

    Added external dependencies

--- default.nix
+++ default.nix
@@ -1,5 +1,4 @@
-{ lib, fetchFromGitHub, rustPlatform }:
-
+{ lib, fetchFromGitHub, rustPlatform, pkg-config, virtualgl, xorg, libevdev }:
 rustPlatform.buildRustPackage rec {
   pname = "mouse-actions";
   version = "v0.4.4";
@@ -11,6 +10,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-02E4HrKIoBV3qZPVH6Tjz9Bv/mh5C8amO1Ilmd+YO5g=";
   };
   cargoHash = "sha256-GRClGC6+3J/YW5jVh7iAb2idD6/PycG854XbBIkfB/c=";
+  buildInputs = [ xorg.libX11 xorg.libXi xorg.libXtst libevdev ];
+  nativeBuildInputs = [ pkg-config virtualgl ];
   meta = with lib; {
     # TODO: better description
     description =
